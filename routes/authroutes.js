44ea847a9d6d88442cedb16e9e3aa7b848c40cb4
Muhammad Suhaib <sohaibamini970@gmail.com>
2026-09-01 15:27:09 +0500

    Update authroutes.js

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { login } = require("../controllers/authcontroller");
+const { login,changePassword } = require("../controllers/authcontroller");
+const {authenticate} = require ("../middleware/authMiddleware")
 
 router.post("/login", login);
+router.put("/change-password",authenticate,changePassword)
 
 //  MUST EXPORT THE ROUTER DIRECTLY
 module.exports = router;
