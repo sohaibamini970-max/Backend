7a2f171a4a0a3251f21b3dbc4a5bfb821f6a3c70
Muhammad Suhaib <sohaibamini970@gmail.com>
2026-09-02 02:31:25 +0500

    Update usercontroller.js

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,12 +1,5 @@
+const pool = require("../config/db");
 const bcrypt = require("bcrypt");
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
-});
 
 /* =========================================================
    ALLOWED ROLES
