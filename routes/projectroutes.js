7ba9b0ce429b557bef9b4db8f003f1fc759e10b8
Muhammad Suhaib <sohaibamini970@gmail.com>
2026-09-01 23:34:30 +0500

    Update projectroutes.js

--- routes/projectroutes.js
+++ routes/projectroutes.js
@@ -3,61 +3,94 @@ const express = require("express");
 const router = express.Router();
 
 const {
+
     authenticate,
+
     requireRole
+
 } = require("../middleware/authMiddleware");
 
 const {
+
     getProjects,
+
     getProjectManagers,
+
     createProject,
+
     assignProject,
+
     unassignProject
+
 } = require("../controllers/projectcontroller");
 
 router.get(
+
     "/",
+
     authenticate,
+
     getProjects
+
 );
 
 router.get(
+
     "/managers",
+
     authenticate,
+
     getProjectManagers
+
 );
 
 router.post(
+
     "/",
+
     authenticate,
+
     requireRole("Executive Manager"),
+
     createProject
+
 );
 
 router.patch(
+
     "/:projectId/assign",
+
     authenticate,
+
     requireRole("Project Manager"),
+
     assignProject
+
 );
 
-
 /*
+
 ============================================================
+
 UNASSIGN PROJECT
+
 ============================================================
 
 ONLY PROJECT MANAGER
+
 */
 
 router.patch(
+
     "/:projectId/unassign",
+
     authenticate,
+
     requireRole("Project Manager"),
+
     unassignProject
+
 );
 
-
 module.exports = router;
-
 
