4a0445c912603ac54f661dec6aecb08073f5bd46
Muhammad Suhaib <sohaibamini970@gmail.com>
2026-09-02 16:38:53 +0500

    Create challengeroutes.js

---
+++ routes/challengeroutes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  authenticate,
+} = require("../middleware/authMiddleware");
+
+const {
+  getTaskChallenges,
+  createTaskChallenge,
+  deleteTaskChallenge,
+} = require("../controllers/challengeController");
+
+/*
+ * Get all challenges for a task.
+ *
+ * Management roles can read.
+ * Assigned Member can read.
+ */
+router.get(
+  "/task/:taskId",
+  authenticate,
+  getTaskChallenges
+);
+
+/*
+ * Add a challenge to a task.
+ *
+ * Backend verifies:
+ * - user is Member
+ * - Member is the task assignee
+ */
+router.post(
+  "/task/:taskId",
+  authenticate,
+  createTaskChallenge
+);
+
+/*
+ * Delete challenge.
+ *
+ * Owner Member or management can delete.
+ */
+router.delete(
+  "/:challengeId",
+  authenticate,
+  deleteTaskChallenge
+);
+
+module.exports = router;
